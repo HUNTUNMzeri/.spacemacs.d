4e7352a59f28da0cb3bb5abab3753b31344202c9
HUNTUNMzeri <huntun@live.com>
2018-09-21 17:31:51 +0800

    Tring to add a function to crete a dairy file

--- layers/HUNTUN/config.el
+++ layers/HUNTUN/config.el
@@ -6,3 +6,17 @@
 (setq OneDrive-Org-Path (concat OneDrive-path "\\org"))
 (setq org-agenda-files (directory-files-recursively OneDrive-Org-Path "\.org$"))
 (setq org-default-notes-file (concat OneDrive-Org-Path "\\Todo\\Todo.org"))
+
+
+;(defun create-dairy-file ()
+;  (let (dairy-file-path)
+;    (setq dairy-file-path
+;          (concat OneDrive-Org-Path "\\Dairy\\"
+;                  (format-time-string "%Y-%m-%d")
+;                  ".org"))
+;    (if (file-exists-p dairy-file-path)
+;      ()
+;      ())))
+;
+;(setq org-capture-templates
+;      '(("d" "Dairy" entry (function ))))
